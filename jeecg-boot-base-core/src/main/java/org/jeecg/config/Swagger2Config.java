53ea7cd656815615b6ad9e01eac3d0d76ea5506d
锐杰 <i@lula.ltd>
2023-09-18 11:05:11 +0800

    修改配置

--- jeecg-boot-base-core/src/main/java/org/jeecg/config/Swagger2Config.java
+++ jeecg-boot-base-core/src/main/java/org/jeecg/config/Swagger2Config.java
@@ -109,14 +109,14 @@ public class Swagger2Config implements WebMvcConfigurer {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 // //大标题
-                .title("JeecgBoot 后台服务API接口文档")
+                .title("LulaBoot 后台服务API接口文档")
                 // 版本号
-                .version("1.0")
+                .version("3.2.5")
 //				.termsOfServiceUrl("NO terms of service")
                 // 描述
                 .description("后台API接口")
                 // 作者
-                .contact(new Contact("北京敲敲云科技有限公司","www.jeccg.com","jeecgos@163.com"))
+                .contact(new Contact("四川花枝科技有限公司","www.lula.ltd","i@lula.ltd"))
                 .license("The Apache License, Version 2.0")
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
                 .build();
